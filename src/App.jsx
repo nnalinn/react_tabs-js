dfe04d5384c9d57bb990f300a77a22de232cdf0a
nnalinn <alina.vysotskayaaa@gmail.com>
2025-03-17 23:22:50 +0200

    add task solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import React, { Component } from "react";
-import "bulma/css/bulma.css";
-import "@fortawesome/fontawesome-free/css/all.css";
-import "./App.scss";
-import { Tabs } from "./components/Tabs/Tabs";
+import React, { Component } from 'react';
+import 'bulma/css/bulma.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+import './App.scss';
+import { Tabs } from './components/Tabs/Tabs';
 
 export const tabs = [
-  { id: "tab-1", title: "Tab 1", content: "Some text 1" },
-  { id: "tab-2", title: "Tab 2", content: "Some text 2" },
-  { id: "tab-3", title: "Tab 3", content: "Some text 3" },
+  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
+  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
+  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
 ];
 
 class App extends Component {
@@ -18,20 +18,26 @@ class App extends Component {
     };
   }
 
-  handleTabSelected = (tabId) => {
+  handleTabSelected = tabId => {
     this.setState({ activeTabId: tabId });
   };
 
   render() {
     const { activeTabId } = this.state;
+
     return (
       <div className="section">
-        <h1 className="title">Selected tab is {tabs.find(tab => tab.id === activeTabId)?.title}</h1>
-        <Tabs tabs={tabs} activeTabId={activeTabId} onTabSelected={this.handleTabSelected} />
+        <h1 className="title">
+          Selected tab is {tabs.find(tab => tab.id === activeTabId)?.title}
+        </h1>
+        <Tabs
+          tabs={tabs}
+          activeTabId={activeTabId}
+          onTabSelected={this.handleTabSelected}
+        />
       </div>
     );
   }
 }
 
 export { App };
-
